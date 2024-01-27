88c94fa65778c06c4c2f2dc37ab56bd17fbb5fc9
TayyabKhalid25 <tayyabkhalid2004@gmail.com>
2024-01-27 20:40:50 +0500

    yeah

--- C++/06_Pointers/Main.cpp
+++ C++/06_Pointers/Main.cpp
@@ -5,15 +5,21 @@ int main()
 {
     // Pointers:
     int num1 = 20;
-    int* var1{ &num1 }, * var2{ nullptr }, * var3, var4; // * before variable name initialises it as pointer. int pointer can only point to int variables.
+    int* var1{ &num1 }, * var2{ NULL }, * var3{}, var4; // * before variable name initialises it as pointer. int pointer can only
+    // point to int variables.
     // All pointers have same size. var4 is regular variable. &num1 gives the address of num1.
     // All pointers are initialised with nullptr by default. Dont cout pointers with nullptr.
-    int* dpointer = new int; //Dynamically made a pointer to a memory slot permanently, until delete <variablename> is used. Scope is global, can be accessed from anywhere.
+    int* dpointer = new int; //Dynamically made a pointer to a memory slot permanently, until delete <variablename> is used.
+    // Scope is global, can be accessed from anywhere.
     *dpointer = 77;  // This is how you access data from a pointer variable.
-    delete dpointer;  // Never use delete twice on a pointer.
-    dpointer = nullptr; // just to play it safe, ensure no random memory is accessed. 0 or NULL can also be used.
+    delete dpointer;  // Never use delete twice on a pointer, it will crash.
+    dpointer = nullptr; // just to play it safe, ensure no random memory is accessed. 0 or NULL or {} can also be used.
+    // Dangling pointers if uninitialised, deleted, or multiple pointers pointing to same address.
+    // Good practice to always intialise properly and after deleting, assign nullptr.
+    // Always check if pointer is not pointing to nullptr to ensure no crash happens.
 
     
     // COMPLETE POINTERS SECTION ABOVE (10:46:06).
 	return 0;
 }
+
