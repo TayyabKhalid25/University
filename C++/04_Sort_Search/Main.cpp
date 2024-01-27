4fe343067c38fde07b5f4d226f78a59e3bf57df8
TayyabKhalid25 <tayyabkhalid2004@gmail.com>
2024-01-27 20:06:26 +0500

    refactored

---
+++ C++/04_Sort_Search/Main.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int array[], int size) {
+
+    // loop to access each array element
+    for (int step = 0; step < size; ++step) {
+
+        // loop to compare array elements
+        for (int i = 0; i < size - step; ++i) {
+
+            // compare two adjacent elements
+            // change > to < to sort in descending order
+            if (array[i] > array[i + 1]) {
+
+                // swapping elements if elements
+                // are not in the intended order
+                int temp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
+            }
+        }
+    }
+}
+
+void insertionSort(int array[], int size) {
+    for (int step = 1; step < size; step++) {
+        int key = array[step];
+        int j = step - 1;
+
+        // Compare key with each element on the left of it until an element smaller than
+        // it is found.
+        // For descending order, change key<array[j] to key>array[j].
+        while (key < array[j] && j >= 0) {
+            array[j + 1] = array[j];
+            --j;
+        }
+        array[j + 1] = key;
+    }
+}
+
+void selectionSort(int array[], int size) {
+    for (int step = 0; step < size - 1; step++) {
+        int min_idx = step;
+        for (int i = step + 1; i < size; i++) {
+
+            // To sort in descending order, change > to < in this line.
+            // Select the minimum element in each loop.
+            if (array[i] < array[min_idx])
+                min_idx = i;
+        }
+
+        // put min at the correct position
+        int temp = array[min_idx];
+        array[min_idx] = array[step];
+        array[step] = temp;
+    }
+}
+
+int linearSearch(int array[], int n, int x) {
+
+    // Going through array sequencially
+    for (int i = 0; i < n; i++)
+        if (array[i] == x)
+            return i;
+    return -1;
+}
+
+int binarySearch(int array[], int x, int size) {
+    int lower = 0;
+    int upper = size - 1;
+    // Repeat until the pointers low and high meet each other
+    while (lower <= upper) {
+        int mid = (lower + upper) / 2;
+
+        if (array[mid] == x)
+            return mid;
+
+        if (array[mid] < x)
+            lower = mid + 1;
+
+        else
+            upper = mid - 1;
+    }
+    return -1;
+}
