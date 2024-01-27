da45a561af0aa423de6a3f31e82ea66b260ff2f1
TayyabKhalid25 <tayyabkhalid2004@gmail.com>
2024-01-27 21:03:29 +0500

    exception handling

--- C++/06_Pointers+Exception_Handling/Main.cpp
+++ C++/06_Pointers+Exception_Handling/Main.cpp
@@ -13,10 +13,29 @@ int main()
     // Scope is global, can be accessed from anywhere.
     *dpointer = 77;  // This is how you access data from a pointer variable.
     delete dpointer;  // Never use delete twice on a pointer, it will crash.
+    // No need to worry about deleting a pointer which points to nullptr, wont crash.
     dpointer = nullptr; // just to play it safe, ensure no random memory is accessed. 0 or NULL or {} can also be used.
     // Dangling pointers if uninitialised, deleted, or multiple pointers pointing to same address.
     // Good practice to always intialise properly and after deleting, assign nullptr.
-    // Always check if pointer is not pointing to nullptr to ensure no crash happens.
+    // We can check if pointer is not pointing to nullptr to ensure no crash happens or use exception handling.
+    if (!(var1 == nullptr))
+        cout << "Address is: " << var1 << endl;
+    else
+        cout << "Doesnt point to anything.\n";
+    if (var2)
+        cout << "Address is: " << var2 << endl;
+    else
+        cout << "Doesnt point to anything.\n";
+    // Exception Handling:
+    try
+    {
+        int* pointer1 = new int[10000000000000];  // We cant allocate large amount of memory for arrays.
+    }
+    catch (const std::exception& ex)
+    {
+        cout << "Error Caught: " << ex.what() << endl;
+    }
+
 
     
     // COMPLETE POINTERS SECTION ABOVE (10:46:06).
