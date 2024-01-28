f2e0c45dda8577355486cc367b05dbfe7c479beb
TayyabKhalid25 <tayyabkhalid2004@gmail.com>
2024-01-28 16:50:53 +0500

    Pointers finished

--- C++/06_Pointers+References/Main.cpp
+++ C++/06_Pointers+References/Main.cpp
@@ -15,6 +15,7 @@ int main()
     delete dpointer;  // Never use delete twice on a pointer, it will crash.
     // No need to worry about deleting a pointer which points to nullptr, wont crash.
     dpointer = nullptr; // just to play it safe, ensure no random memory is accessed. 0 or NULL or {} can also be used.
+
     // Dangling pointers if uninitialised, deleted, or multiple pointers pointing to same address.
     // Good practice to always intialise properly and after deleting, assign nullptr.
     // We can check if pointer is not pointing to nullptr to ensure no crash happens or use exception handling.
@@ -26,6 +27,7 @@ int main()
         cout << "Address is: " << var2 << endl;
     else
         cout << "Doesnt point to anything.\n";
+
     // Exception Handling:
     try
     {
@@ -37,6 +39,7 @@ int main()
     }
     // We can also use std::nothrow as alternative to try/catch.
     int* pointer1 = new(nothrow) int[10000000000];
+
     // Dynamic Arrays:
     // These are different from simple arrays in working as well, cant use std::size or range based for loops.
     double* arr = new double[10];  
@@ -47,9 +50,18 @@ int main()
     delete[] arr;  // You cant delete multiple memories with one delete.
     delete[] points;
     delete[] arr1;
-    
-    
-    // COMPLETE POINTERS SECTION ABOVE (12:12:40).
+
+    // References:
+    int num3 = 22;
+    int& ref1 = num1;
+    double num2 = 19.2;
+    double& ref2{ num2 };
+    const int& ref3 = num3;  // Can't change referenced variable's value, only use it.
+    // Major difference b/w pointers is:
+    // int& ref4;  // References MUST be initialised at declaration.
+    // &ref1 = num3;  // References CAN'T be changed to other variables, something like const pointers.
+    // No need to 'dereference'.
+
 	return 0;
 }
 
---
+++ C++/07_OOP_Introduction/Main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+
+
+	// Resume from 20:15:40
+	return 0;
+}
