d1a8739cc7404394c40317c7dfc48d65b8f1c761
TayyabKhalid25 <tayyabkhalid2004@gmail.com>
2024-03-21 22:46:48 +0500

    assignment 3 creat

---
+++ C++/Semester_2/OOP_Assignment_3/23L-0501_A3.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+class EvalReport {
+private:
+
+public:
+
+
+};
+
+int main()
+{
+	
+
+	return 0;
+}
+//std::cout << std::left << std::setw(10) << "Roll No."
+//<< std::setw(20) << "Name"
+//<< std::setw(5) << "Q1"
+//<< std::setw(5) << "Q2"
+//<< std::setw(5) << "Q3"
+//<< std::setw(5) << "Q4"
+//<< std::setw(5) << "A1"
+//<< std::setw(5) << "A2"
+//<< std::setw(5) << "A3"
+//<< std::setw(5) << "A4"
+//<< std::setw(10) << "Total" << std::endl;
+//
+//std::cout << std::left << std::setw(10) << "15L-4308"
+//<< std::setw(20) << "Arham Fatima"
+//<< std::setw(5) << "10"
+//<< std::setw(5) << "0"
+//<< std::setw(5) << "0"
+//<< std::setw(5) << "0"
+//<< std::setw(5) << "41"
+//<< std::setw(5) << "31"
+//<< std::setw(5) << "0"
+//<< std::setw(5) << "54"
+//<< std::setw(10) << "" << std::endl;
+//
